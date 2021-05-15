17919ac64b78e200b5a5a976e01d2d802b47c151
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-05-15 11:42:42 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,19 @@ import {
   TaskDialogComponent,
   TaskDialogResult,
 } from './task-dialog/task-dialog.component';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/firestore';
+import { BehaviorSubject } from 'rxjs';
+
+const getObservable = (collection: AngularFirestoreCollection<Task>) => {
+  const subject = new BehaviorSubject([]);
+  collection.valueChanges({ idField: 'id' }).subscribe((val: Task[]) => {
+    subject.next(val);
+  });
+  return subject;
+};
 
 @Component({
   selector: 'app-root',
@@ -14,11 +26,9 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  todo = this.store.collection('todo').valueChanges({ idField: 'id' });
-  inProgress = this.store
-    .collection('inProgress')
-    .valueChanges({ idField: 'id' });
-  done = this.store.collection('done').valueChanges({ idField: 'id' });
+  todo = getObservable(this.store.collection('todo'));
+  inProgress = getObservable(this.store.collection('inProgress'));
+  done = getObservable(this.store.collection('done'));
 
   constructor(private dialog: MatDialog, private store: AngularFirestore) {}
 
@@ -26,6 +36,13 @@ export class AppComponent {
     if (event.previousContainer === event.container) {
       return;
     }
+    const item = event.previousContainer.data[event.previousIndex];
+    this.store.firestore.runTransaction(() => {
+      return Promise.all([
+        this.store.collection(event.previousContainer.id).doc(item.id).delete(),
+        this.store.collection(event.container.id).add(item),
+      ]);
+    });
     transferArrayItem(
       event.previousContainer.data,
       event.container.data,
@@ -43,16 +60,14 @@ export class AppComponent {
       },
     });
     dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
-      const dataList = this[list];
-      const taskIndex = dataList.indexOf(task);
       //not cancel situation
       if (result !== undefined) {
         if (result.delete) {
           //delete
-          dataList.splice(taskIndex, 1);
+          this.store.collection(list).doc(task.id).delete();
         } else {
           //change
-          dataList[taskIndex] = task;
+          this.store.collection(list).doc(task.id).update(task);
         }
       }
     });
@@ -68,7 +83,7 @@ export class AppComponent {
     dialogRef.afterClosed().subscribe((result: TaskDialogResult) => {
       //not cancel situation
       if (result !== undefined) {
-        this.todo.push(result.task);
+        this.store.collection('todo').add(result.task);
       }
     });
   }
